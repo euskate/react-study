29f8a1af2185ea80185d49c35a5afc1af43a0f6d
euskate <euskate07@gmail.com>
2023-09-07 06:33:40 +0900

    7-3 movie app part one get data

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import { useCallback, useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    const json = await response.json()
+    setMovies(json.data.movies)
+    setLoading(false)
+  }
+
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        // console.log(json)
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
+    getMovies()
+  }, [])
+  console.log(movies);
   return (
     <div>
-      <h1>The Coin! {coins.length} </h1>
+      <h1>Movie</h1>
       {loading ? (
-        "loading..."
+        <h1>"loading..."</h1>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name}, ({coin.symbol}): ${coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
+        <div>
+          {movies.map(movie => <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>{movie.genres ?? movie.genres.map(g => <li>{g}</li>)}</ul>
+            <img src={movie.medium_cover_image} alt="movie_image" />
+          </div>)}
+        </div>)}
     </div>
   );
 }
---
+++ src/Coin.js
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        // console.log(json)
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coin! {coins.length} </h1>
+      {loading ? (
+        "loading..."
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name}, ({coin.symbol}): ${coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
+
+export default App;
