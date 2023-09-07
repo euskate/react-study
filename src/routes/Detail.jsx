636460fd0b39fdce22a82e096f88f9d51c2550b0
euskate <euskate07@gmail.com>
2023-09-08 07:53:22 +0900

    modify detail

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -3,16 +3,15 @@ import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
-  useState;
-  const getMovie = async () => {
-    const response = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    console.log(response);
-  };
-  useEffect(() => {
-    getMovie();
-  }, []);
+  // const getMovie = async () => {
+  //   const response = await (
+  //     await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+  //   ).json();
+  //   console.log(response);
+  // };
+  // useEffect(() => {
+  //   getMovie();
+  // }, []);
   return (
     <div>
       <h1>Detail {id}</h1>
