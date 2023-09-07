f493ffc89f66feaeddec93d20728df54bd012255
euskate <euskate07@gmail.com>
2023-09-08 04:23:20 +0900

    7-6 useParam

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Detail() {
-  return <div>Detail</div>;
+  const { id } = useParams();
+  useState;
+  const getMovie = async () => {
+    const response = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(response);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      <h1>Detail {id}</h1>
+      <p></p>
+    </div>
+  );
 }
 
 export default Detail;
